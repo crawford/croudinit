396dc4f87007d9753767c052d380838b8e09493d
Alex Crawford <alex.crawford@coreos.com>
2014-08-19 18:27:06 -0700

    Add Warning to Reporter

--- validator/report/report.go
+++ validator/report/report.go
@@ -1,24 +1,25 @@
 package report
 
+type entry struct {
+	line    int
+	message string
+}
+
 type Report struct {
-	errors []struct {
-		line    int
-		message string
-	}
+	errors   []entry
+	warnings []entry
 }
 
 func (r *Report) Error(line int, message string) {
-	r.errors = append(r.errors, struct {
-		line    int
-		message string
-	}{line, message})
+	r.errors = append(r.errors, entry{line, message})
+}
+
+func (r *Report) Warning(line int, message string) {
+	r.warnings = append(r.warnings, entry{line, message})
 }
 
 func NewReport() *Report {
 	return &Report{
-		errors: make([]struct {
-			line    int
-			message string
-		}, 0),
+		errors: make([]entry, 0),
 	}
 }
--- validator/rules/rules.go
+++ validator/rules/rules.go
@@ -2,6 +2,7 @@ package rules
 
 type Reporter interface {
 	Error(line int, message string)
+	Warning(line int, message string)
 }
 
 type Rule func(contents []byte, reporter Reporter)
